5d5c1d65a0dc805c233e3d730e94b52af4b95ab8
copslock <34976676+copslock@users.noreply.github.com>
2018-09-29 08:45:53 +0000

    Update boot.rb

--- examples/boot.rb
+++ examples/boot.rb
@@ -1,26 +1,41 @@
-$:.unshift File.expand_path("../../lib", __FILE__)
-require "pagseguro"
-
-I18n.locale = "pt-BR"
-
-PagSeguro.configure do |config|
-  # You can setup with Application Credentials trough TOKEN and EMAIL
-  #   config.token = ENV.fetch("PAGSEGURO_TOKEN")
-  #   config.email = ENV.fetch("PAGSEGURO_EMAIL")
-  #
-  # OR with Application Credentials through APP_ID and APP_KEY
-  #   config.app_id = ENV.fetch("PAGSEGURO_APP_ID")
-  #   config.app_key = ENV.fetch("PAGSEGURO_APP_KEY")
-  #
-  #
-  # Ps: You can set the credentials when you to call some service like:
-  #   For Account credentials based on EMAIL and TOKEN
-  #     - PagSeguro::ApplicationCredentials.new("appteste_1", "TESTE11111")
-  #
-  #   OR
-  #
-  #   For Application credentials based on APP_KEY and APP_ID
-  #     - PagSeguro::AccountCredentials.new("user@example.com", "token")
-
-  config.environment = :sandbox
+#!/usr/bin/env ruby
+infile = ARGV[0] 
+outfile = ARGV[1]
+start = ARGV[2]
+finish = ARGV[3]
+puts("#{start} - #{finish}");
+#bfc00000: 100000ff 00000000 100000fd 00000000    ................
+f = File.new(infile, "r")
+fout = File.new(outfile, "w")
+writeFlag = false
+while(line = f.gets)
+ splitLine = line.strip.split(/\s+/, 6)
+ if(splitLine.size == 6)
+  address=splitLine[0]
+  address=address[0,8]
+  hex1=splitLine[1]
+  hex2=splitLine[2]
+  hex3=splitLine[3]
+  hex4=splitLine[4]
+  ascii=splitLine[5]
+  str = "#{address} #{hex1}#{hex2}#{hex3}#{hex4} #{ascii}"
+  if(address.downcase == start.downcase)
+   puts("Found start: #{str}")
+   writeFlag = true;
+  end
+ if(writeFlag == true)
+   unhex1=[hex1].pack("H*").reverse
+   unhex2=[hex2].pack("H*").reverse
+   unhex3=[hex3].pack("H*").reverse
+   unhex4=[hex4].pack("H*").reverse
+   unhex = unhex1 + unhex2 + unhex3 + unhex4
+   fout.write(unhex)
+  end
+  if(address.downcase == finish.downcase)
+   puts("Found finish: #{str}")
+   fout.close();
+   exit
+  end
+ end
 end
+f.close()
